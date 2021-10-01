027e94cce390d662f22a4a7df17b5786e208f8a0
michaelzhu0819 <89995944+michaelzhu0819@users.noreply.github.com>
2021-10-01 10:22:06 -0600

    Update RPG_GAME_MENU_CODE.py
    Deleted trailing whitespaces, readjusted line 6-9 to fit into the 79 character space limit, changed the multiline indent to fit the requirements of PEP8

--- RPG_GAME_MENU_CODE.py
+++ RPG_GAME_MENU_CODE.py
@@ -1,20 +1,21 @@
 # Python simple menu - text based adventure game
 # Michael Zhu
 # Sept 21st, 2021
-# CS30 
+# CS30
 # Ms.Cotcher
-""" This is the menu for a game where you are an adventurer trying to escape a 
-wasteland, you can commit combat to potential enemies, collect potential materials,
-and try to navigate through this endless map"""
+""" This is the menu for a game where you are an adventurer trying to escape a
+wasteland with a locked door, navigate through the biome with four movements,
+encounter and defeat enemies, collect resources, and try to find the key to
+the door"""
 
 # These variables set up the catagories for actions and the coordinate system
 x = 0
 y = 0
 general_action = ["explore", "combat"]
-explore = ["forward", "backward", "left", "right", "collect", "stop"]
+explore = ["forward", "backward", "left", "right", "quit"]
 combat = ["attack", "hide", "defend"]
 
-""" The while loop that makes sure the general menu is running constantly until 
+""" The while loop that makes sure the general menu is running constantly until
 quit is inputted, during this you can either select explore or combat"""
 while True:
     print("\nyou can do the following catagory of actions:")
@@ -22,44 +23,43 @@ while True:
         print (i)
     print("side note: you can type quit to quit anytime\n")
     cata_input = input("which one do you choose?\n")
-    
-    """ The section is what happens when you select explore, it constantly loops
-    and ask for input until you type quit which will return to the general 
-    menu"""
+
+    """ The section is what happens when you select explore, it constantly
+    loops and ask for input until you type quit which will return to the
+    general menu"""
     if cata_input.lower() == "explore":
         while True:
-            print("\nu can do either")
+            print("\nu can go either")
             for direction in explore:
                 print(direction)
             direction_input = input("which one do you choose?\n")
-            
-            # The coordinate system 
+
+            # The coordinate system
             if direction_input.lower() in explore:
+                print("\n" + direction_input.lower() + "!")
                 coord_change = direction_input.lower()
                 if coord_change == explore[0]:
                     y += 1
                     print("your y value increased by one, " +
-                    "your coord now is " + str(x)+" ," + str(y))
+                          "your coord now is " + str(x)+" ," + str(y))
                 elif coord_change == explore[1]:
                     y -= 1
                     print("your y value decreased by one, " +
-                    "your coord now is " + str(x)+" ," + str(y))
+                          "your coord now is " + str(x)+" ," + str(y))
                 elif coord_change == explore[2]:
                     x -= 1
                     print("your x value decreased by one, " +
-                    "your coord now is " + str(x) + " ," + str(y))
+                          "your coord now is " + str(x) + " ," + str(y))
                 elif coord_change == explore[3]:
                     x += 1
                     print("your x value increased by one, " +
-                    "your coord now is " + str(x) + " ," + str(y))
+                          "your coord now is " + str(x) + " ," + str(y))
                 elif direction_input.lower() == explore[4]:
-                    print(direction_input.lower() + "!")
-                elif direction_input.lower() == explore[5]:
                     print("going back to the main menu\n")
                     break
-            else:    
+            else:
                 print("invalid action!")
-    
+
     # Happens when you select combat in the general menu
     elif cata_input.lower() == "combat":
         print("\nu can do either")
@@ -70,7 +70,7 @@ while True:
             print("\n" + combat_input.lower() + "!")
         else:
             print("invalid action!")
-    
+
     # Breaks the loop when inputted quit and accepts wrong inputs
     elif cata_input.lower() == "quit":
         break
